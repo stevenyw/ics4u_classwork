b74fd182f23df0b9b5e276664ac751db17e031c1
steven <steven.wong26@ycdsbk12.ca>
2025-04-14 09:57:28 -0400

    Update recursion.cs

--- recursion.cs
+++ recursion.cs
@@ -20,7 +20,7 @@ class Program
 
 // It cycles from 4 to 0 until it reaches 0, and then it goes back up and does the recursive step
 // be(n=0) = return 1;
-// be(n=1) = return 1 * 1 = 1;
-// be(n=2) = return 2 * 1 = 2;
-// be(n=3) = return 3 * 2 = 6;
+// be(n=1) = return 1 x 1 = 1;
+// be(n=2) = return 2 x 1 = 2;
+// be(n=3) = return 3 x 2 = 6;
 // be (n=4) = return 4 x 6 = 24;
